97f998c94cd558bc7fb701da1b435f6b2d1627db
carlosalviranatal <carlosalviranatal@DESKTOP-A4A091C.localdomain>
2023-08-17 11:39:43 -0500

    Added delete button functionality

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -2,6 +2,7 @@ import { fetchAllPlayers } from '../API'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import DeletePlayer from './DeletePlayer'
 
 export default function AllPlayers() {
   const [players, setPlayers] = useState([])
@@ -9,6 +10,7 @@ export default function AllPlayers() {
   const [error, setError] = useState(null)
   const [searchParam, setSearchParam] = useState('')
   const navigate = useNavigate()
+
   useEffect(() => {
     async function receiveAllPlayers() {
       const APIData = await fetchAllPlayers()
@@ -27,10 +29,12 @@ export default function AllPlayers() {
       )
     : players
 
+  const handlePlayerDelete = (playerId) => {
+    setPlayers(players.filter((player) => player.id !== playerId))
+  }
   return (
     <>
       <h1>All Players</h1>
-
       <div>
         <label>
           Search
@@ -44,20 +48,15 @@ export default function AllPlayers() {
       </div>
 
       <div>
-        {playersToDisplay.map((player) => {
-          return (
-            // eslint-disable-next-line react/jsx-key
-            <div>
-              <h4 key={player.id}>{player.name}</h4>
-              <button
-                key={`button ${player.id}`}
-                onClick={() => navigate(`/${player.id}`)}
-              >
-                See Details!
-              </button>
-            </div>
-          )
-        })}
+        {playersToDisplay.map((player) => (
+          <div key={player.id}>
+            <h4>{player.name}</h4>
+            <button onClick={() => navigate(`/${player.id}`)}>
+              See Details!
+            </button>
+            <DeletePlayer playerId={player.id} onDelete={handlePlayerDelete} />
+          </div>
+        ))}
       </div>
     </>
   )
